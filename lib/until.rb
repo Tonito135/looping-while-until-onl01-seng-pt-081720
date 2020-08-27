c204761ebf2e25c73f2ed48515e1514f83664d7a
Anthony Hernandez <anthonyh2020@gmail.com>
2020-08-27 22:10:06 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,8 @@
 def using_until(phrase = "Wingardium Leviosa")
   levitation_force = 6
-  until levitation_force == 10
-  puts phrase
-  levitation_force += 1 
+    until levitation_force == 10
+    puts phrase
+    levitation_force += 1 
+  end
 end
 
